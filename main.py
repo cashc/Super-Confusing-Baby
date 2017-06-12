92ded6a68df0ccc163eef9ddd29dbfe514884159
cashc <cashc@cash.me>
2017-06-12 17:09:31 -0400

    GET and POST routes set up, returning index.html.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['POST'])
+def parse_request():
+    return 'Thanks for making a POST request!'
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
