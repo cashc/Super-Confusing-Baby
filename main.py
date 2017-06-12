e500e929946609a5b56d7f9cec8a9c49ff476cde
Ryan Feldman <ryan@Ryans-MacBook-Pro.local>
2017-06-12 17:32:17 -0400

    Added routes for testing. Basic SMS works fine. - Ryan

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'troll.talk.sms'
+app.config['MAIL_PASSWORD'] = 'superbaby'
+
+mail = Mail(app)
 
 @app.route('/', methods=['POST'])
 def parse_request():
@@ -11,6 +19,11 @@ def parse_request():
 def home():
     return render_template('index.html')
 
+@app.route('/test')
+def index():
+    msg = Message('Hello', sender='troll.talk.sms@gmail.com', recipients=['2484082851@vtext.com'])
+    mail.send(msg)
+    return 'Message Sent!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
